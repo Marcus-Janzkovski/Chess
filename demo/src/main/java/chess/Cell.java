4b62f66e35a17ccc64ad5bf93d329553e915e030
Marcus-Janzkovski <mvjanzkovski@gmail.com>
2024-10-14 12:19:52 -0300

    class refact

--- demo/src/main/java/chess/Cell.java
+++ demo/src/main/java/chess/Cell.java
@@ -5,47 +5,68 @@ public class Cell {
     private Celltype celltype ;
 
     private int celrow;
-    private char celcol;
+    private int celcol;
 
-    public Cell(char inputCol,int inputRow ) {
+    private int chessrow;
+    private char chesscol;
+
+    public Cell(int inputCol,int inputRow ) {
 
         this.celltype = Celltype.EMPTY;
 
-        setCelrow(inputRow);
         setCelcol(inputCol);
+
+        setCelrow(inputRow);
 
     }
 
     
-    
-
     public Celltype getCelltype() {
         return celltype;
     }
     public void setCelltype(Celltype celltype) {
         this.celltype = celltype;
     }
+
+    public int getChessrow() {
+        return chessrow;
+    }
+
+    private void setChessrow(int chessrow) { //private, to avoid conflicts
+        this.chessrow = chessrow;
+    }
+
+    public char getChesscol() {
+        return chesscol;
+    }
+
+    private void setChesscol(char chesscol) { //private, to avoid conflicts
+        this.chesscol = chesscol;
+    }
+
     public int getCelrow() {
         return celrow;
     }
-    public final void setCelrow(int celrow) {
-        this.celrow = celrow;
-    }
-    public char getCelcol() {
-        return this.celcol;
-    }
-    
-    
-    public final int setCelcol(char input) { // Retorna -1 em caso de input inválido
-        
-        if (input < 'A' || input > 'H' ){
-            this.celcol = 'X';
-            return -1;
-        } else {
-            this.celcol = input;
-            return 1;
-        }
-    }
- 
 
+    public final void setCelrow(int celrow) { //Convert the matrix position to Chess abstract pos
+        this.celrow = celrow;
+
+        setChessrow(celrow + 1); //Board starts on 1 and matrix starts on 0
+
+    }
+
+    public int getCelcol() {
+        return celcol;
+    }
+
+    public final void setCelcol(int celcol) { //Convert the matrix position to Chess abstract pos
+
+        this.celcol = celcol;
+
+        char equivalentChar = (char) (celcol + 65); //Convert matrix col into ASCII -> A = 65, B = 66 .....
+
+        setChesscol(equivalentChar);
+
+    }
+   
 }
--- demo/src/test/java/chess/CellTest.java
+++ demo/src/test/java/chess/CellTest.java
@@ -7,30 +7,55 @@ import org.junit.Test;
 
 public class CellTest {
 
-    @Test
-    public void constructorTestrow(){
-        Cell cell = new Cell('A', 1);
+    @Test   
+    public void checkChessCol1(){
+        Cell cell = new Cell(0,0);
 
-        assertEquals(1, cell.getCelrow());
+        assertEquals('A', cell.getChesscol());
+
+    }
+
+    @Test   
+    public void checkChessCol2(){
+        Cell cell = new Cell(1,0);
+
+        assertEquals('B', cell.getChesscol());
 
     }
 
     @Test
-    public void constructorTestcol(){
-        Cell cell = new Cell('A', 1);
+    public void checkChessRow1(){
+        Cell cell = new Cell(0,0);
 
-        assertEquals(cell.getCelcol(), 'A');
+        assertEquals(1, cell.getChessrow());
 
     }
 
     @Test
-    public void constructorTestWrongCol(){
-        Cell cell = new Cell('v', 1);
+    public void checkChessRow2(){
+        Cell cell = new Cell(0,3);
 
-        assertEquals(cell.getCelcol(), 'X');
+        assertEquals(4, cell.getChessrow());
 
     }
 
+    @Test
+    public void checkCellEmpty(){
+        Cell cell = new Cell(0,3);
+
+        assertEquals(Celltype.EMPTY, cell.getCelltype());
+
+    }
+
+    @Test
+    public void checkCellSetType(){
+        Cell cell = new Cell(0,3);
+
+        cell.setCelltype(Celltype.BISHOP);
+
+        assertEquals(Celltype.BISHOP, cell.getCelltype());
+
+    }
 
 
 }
