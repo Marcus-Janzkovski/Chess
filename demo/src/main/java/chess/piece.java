71aee8223a839ce0132d40e42024700423b21a99
Marcus-Janzkovski <mvjanzkovski@gmail.com>
2024-10-14 14:12:45 -0300

    inicio da implementação das peças

---
+++ demo/src/main/java/chess/piece.java
@@ -0,0 +1,15 @@
+package chess;
+
+public class piece {
+
+    private boolean ingame;
+
+    public boolean isIngame() {
+        return ingame;
+    }
+
+    public void setIngame(boolean ingame) {
+        this.ingame = ingame;
+    }
+    
+}
