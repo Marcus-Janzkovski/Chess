2451275f27738bd0c2dc1cb69e6d058abfd2b6c0
Marcus-Janzkovski <mvjanzkovski@gmail.com>
2024-11-06 00:58:46 -0300

    prepare for BoardRefactor

--- demo/src/main/java/chess/Board.java
+++ demo/src/main/java/chess/Board.java
@@ -5,23 +5,26 @@ public class Board {
 
     private Cell[][] Cells;
 
-    final int ROW_COUNT, COL_COUNT;
-
     public Board(int maxRow, int maxCol){
 
-
-        this.ROW_COUNT = maxRow;
-        this.COL_COUNT = maxCol;
         this.Cells = new Cell[maxRow][maxCol];
 
     
-        for(int i = 0;i < ROW_COUNT;i++){
-            for(int j = 0; j < COL_COUNT;j++){
+        for(int i = 0;i < maxRow;i++){
+            for(int j = 0; j < maxCol;j++){
 
                 this.Cells[i][j] = new Cell(i, j); 
 
             }     
         }
+    }
+
+    public Cell[][] getCells() {
+        return Cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+        Cells = cells;
     }
 
 
