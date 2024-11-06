af8a3a91cecbdb0f3c71e2bafe96799e1f15fce3
Marcus-Janzkovski <mvjanzkovski@gmail.com>
2024-11-05 23:38:30 -0300

    cellRefactor

--- demo/src/test/java/chess/CellTest.java
+++ demo/src/test/java/chess/CellTest.java
@@ -4,44 +4,124 @@ package chess;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import chess.Pieces.Bishop;
+import chess.Pieces.Color;
+
 
 public class CellTest {
 
+    @Test
+    public void testGetCelcol() {
+        Cell cell = new Cell(2,3);
 
-//Check Contruct class
-
-    @Test   
-    public void checkChessCol1(){
-        Cell cell = new Cell(0,0);
-
-        assertEquals('A', cell.getChesscol());
-
-    }
-
-    @Test   
-    public void checkChessCol2(){
-        Cell cell = new Cell(1,0);
-
-        assertEquals('B', cell.getChesscol());
+        assertEquals(2,cell.getCelcol());
 
     }
 
     @Test
-    public void checkChessRow1(){
-        Cell cell = new Cell(0,0);
+    public void testGetCelrow() {
+        
+        Cell cell = new Cell(2,3);
 
-        assertEquals(1, cell.getChessrow());
+        assertEquals(3,cell.getCelrow());
 
     }
 
     @Test
-    public void checkChessRow2(){
-        Cell cell = new Cell(0,3);
+    public void testGetChesscol() {
 
-        assertEquals(4, cell.getChessrow());
+        Cell cell = new Cell(2,3);
+
+        assertEquals('C',cell.getChesscol());
+    }
+
+    @Test
+    public void testGetChessrow() {
+        
+        Cell cell = new Cell(2,3);
+
+        assertEquals('5',cell.getChessrow());
+
 
     }
 
+   
 
+    @Test
+    public void testSetCelcol() {
+
+        Cell cell = new Cell(2,3);
+
+        cell.setCelcol(5);
+
+        assertEquals(5,cell.getCelcol());
+
+
+    }
+
+    @Test
+    public void testSetCelrow() {
+        
+        Cell cell = new Cell(2,3);
+
+        cell.setCelrow(5);
+
+        assertEquals(5,cell.getCelrow());
+
+    }
+
+    @Test
+    public void testSetChesscol() {
+        Cell cell = new Cell(2,3);
+
+        cell.setChesscol('A');
+
+        assertEquals('A',cell.getChesscol());
+
+
+    }
+
+    @Test
+    public void testSetChessrow() {
+
+        Cell cell = new Cell(2,3);
+
+        cell.setChessrow('1');
+
+        assertEquals('1',cell.getChessrow());
+        
+    }
+
+       @Test
+
+        public void testSetandGetPiece() {
+
+        Cell cell = new Cell(2,3);
+        Bishop bishop = new Bishop(Celltype.BISHOP, Color.BLACK);
+        cell.setPiece(bishop);
+
+        assertEquals(Celltype.BISHOP, cell.getPiece().getName());
+
+    }
+
+    @Test
+    public void testIsEmpty1() {
+
+        Cell cell = new Cell(2,3);
+        Bishop bishop = new Bishop(Celltype.BISHOP, Color.BLACK);
+        cell.setPiece(bishop);
+
+        assertEquals(false, cell.isEmpty());
+
+    }
+
+    @Test
+    public void testIsEmpty2() {
+
+        Cell cell = new Cell(2,3);
+
+        assertEquals(true, cell.isEmpty());
+
+    }
 
 }
